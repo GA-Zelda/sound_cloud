e48f2014254b4ddf05e52a7f87581b290cd137ef
nielssorensen <niels.ws@gmail.com>
2015-02-02 19:52:45 -0500

    added sessions

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,22 @@
+class SessionController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      #redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    #redirect_to '/'
+  end
+
+end

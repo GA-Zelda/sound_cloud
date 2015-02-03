7d44cae1d4c248dc585ae9aa1970b878971f9f7c
gretchenziegler <gretchenziegler@gmail.com>
2015-02-02 22:59:18 -0500

    edited tracks controller for index method, and edited track index

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,19 @@
 class TracksController < ApplicationController
+
+	# create a new track
+	# POST "/users/:user_id/tracks"
+	def create
+		user_id = params[:user_id]
+
+	end
+
+	# get a list of users saved tracks
+	# GET '/users/:user_id/tracks'
+	def index
+		@user = User.find(params[:id])
+		@name = @user.name
+		@user_id = @user.id
+		@tracks = Track.where(user_id: params[:id])
+	end
+
 end
